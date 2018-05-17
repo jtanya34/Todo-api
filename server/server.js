1a5bbf8aedc941cd8648a1ec6c6b83401644d261
tanya <jtanya34@gmail.com>
2018-05-17 10:19:54 +0530

    Heroku setup

--- server/server.js
+++ server/server.js
@@ -4,11 +4,13 @@ var bodyParser=require('body-parser');
 
 const {ObjectID}=require('mongodb');
 var {mongoose}=require('./db/mongoose.js');
+
 var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
 
 var app=express();
 const port =process.env.PORT||3000;
+
 app.use(bodyParser.json());
 
 app.post('/todos',(req,res)=>{
@@ -24,35 +26,34 @@ res.status(400).send(e);
 });
 
 
-app.get('/todos',(req,res)=>{
-	todo.find().then(()=>{
-res.send({
-	todo
-});
-	},(e)=>{
-res.status(400).send(e);
-	})
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
 
 
-app.get('/todos/:id',(req,res)=>{
-	var id=req.params.id;
-	if(!ObjectID.isValid(id)){
-	return res.status(404).send();
-}
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
 
-Todo.findById(id).then((todo)=>{
-	if (!todo){
-		return res.status(404).send();
-	}
-	res.send({todo});
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
 
-}).catch((e)=>{
-	res.status(400).send(e)
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
-});
 
 
 app.listen(port,()=>{
