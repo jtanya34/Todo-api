7d6f8c9455670bd064ebea606f00afec2388ff47
tanya <jtanya34@gmail.com>
2018-05-17 23:08:57 +0530

    GET /user/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ var {mongoose}=require('./db/mongoose');
 
 var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
-
+var{authenticate}=require('./middleware/authenticate');
 var app=express();
 const port =process.env.PORT||3000;
 
@@ -122,6 +122,10 @@ res.header('x-auth',token).send(user);
 
 
 
+
+app.get('/users/me',authenticate,(req,res)=>{
+	res.send(req.user);
+});
 
 
 app.listen(port,()=>{
