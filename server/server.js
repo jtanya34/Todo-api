e82f582b459e7a1676e29182e2c7bfa668d34c1a
tanya <jtanya34@gmail.com>
2018-05-17 10:51:24 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var bodyParser=require('body-parser');
 
 
 const {ObjectID}=require('mongodb');
-var {mongoose}=require('./db/mongoose.js');
+var {mongoose}=require('./db/mongoose');
 
 var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
