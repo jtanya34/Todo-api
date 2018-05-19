dad36b99fc7f721d62bc1e244d3a7eed9f688712
tanya <jtanya34@gmail.com>
2018-05-19 23:25:23 +0530

    mongoose Promis

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose=require('mongoose');
 
-mongoose.Promise=global.Promise;
+mongoose.Promise=Promise;
 mongoose.connect(process.env.MONGOLAB_URI ||'mongodb://localhost/TodoApp');
 
 module.exports={mongoose};
